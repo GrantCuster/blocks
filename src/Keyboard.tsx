dd11dce6f13ac99936a113676a919f79f1967694
Grant Custer <grantcuster@gmail.com>
2025-04-08 12:46:03 -0400

    improvements

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,11 +1,18 @@
 import { useAtom } from "jotai";
 import { useEffect } from "react";
-import { BlockIdsAtom, BlockMapAtom, StateRefAtom } from "./atoms";
+import {
+  BlockIdsAtom,
+  BlockMapAtom,
+  SelectedBlockIdsAtom,
+  StateRefAtom,
+} from "./atoms";
+import { v4 as uuid } from "uuid";
 
 export function Keyboard() {
   const [stateRef] = useAtom(StateRefAtom);
   const [, setBlockIds] = useAtom(BlockIdsAtom);
   const [, setBlockMap] = useAtom(BlockMapAtom);
+  const [, setSelectedBlockIds] = useAtom(SelectedBlockIdsAtom);
 
   function handleDeleteSelectedBlocks() {
     if (stateRef.selectedBlockIds.length > 0) {
@@ -23,19 +30,38 @@ export function Keyboard() {
   }
 
   function handleDuplicateBlocks() {
-    // TODO: fill in
+    const newBlockMap = { ...stateRef.blockMap };
+    const newBlockIds = [...stateRef.blockIds];
+    let newSelectedBlockIds: string[] = []
+    stateRef.selectedBlockIds.forEach((id) => {
+      const block = stateRef.blockMap[id];
+      if (block) {
+        const newBlock = {
+          ...block,
+          id: uuid(),
+          x: block.x + 16,
+          y: block.y + 16,
+        };
+        newBlockMap[newBlock.id] = newBlock;
+        newBlockIds.push(newBlock.id);
+        newSelectedBlockIds.push(newBlock.id);
+      }
+    });
+    setBlockMap(newBlockMap);
+    setBlockIds(newBlockIds);
+    setSelectedBlockIds(newSelectedBlockIds);
   }
 
   useEffect(() => {
-    // keyboard shortcuts
+    let isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
     function handleKeyDown(event: KeyboardEvent) {
-      // TODO: update to work
-      const isCmdOrCtrl = event.altKey;
+      const isCmdOrCtrl = isMac ? event.metaKey : event.ctrlKey;
       if (event.key === "Backspace") {
         event.preventDefault();
         handleDeleteSelectedBlocks();
       }
       if (isCmdOrCtrl && event.key === "d") {
+        event.preventDefault();
         handleDuplicateBlocks();
       }
     }
