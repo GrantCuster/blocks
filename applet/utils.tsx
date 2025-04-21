4c3e19b7092c23485870f902dbe94d5581ef98c3
Grant Custer <grantcuster@gmail.com>
2025-04-21 11:21:52 -0400

    applet update

--- applet/utils.tsx
+++ applet/utils.tsx
@@ -44,7 +44,7 @@ export function blockIntersectBlocks(
 export async function appletResolveImage(src: string) {
   if (window.location.origin.includes("usercontent.goog")) {
     // const noStartingSlash = src.startsWith("/") ? src.slice(1) : src;
-    const ghPagesSource = "https://grantcuster.github.io/block-applet-test" + src;
+    const ghPagesSource = "https://grantcuster.github.io/block-applet-test/" + src;
     const fetched = await fetch(ghPagesSource);
     const blob = await fetched.blob();
     const url = URL.createObjectURL(blob);
